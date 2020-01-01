5e4de75a0466ac56c3a0e4c4175d77dde7acee7f
Puthiyavan <NPuthiyavan@ashleyfurniture.com>
2020-01-01 15:43:05 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Assignment2Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
